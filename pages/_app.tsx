6b854997b2de2022ee25d8056497a74c84870826
Julian Klummer <secure@julianklummer.de>
2022-12-31 21:03:05 +0100

    add scss support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,11 @@ import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <>
       <header>
         <Navigation />
       </header>
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
