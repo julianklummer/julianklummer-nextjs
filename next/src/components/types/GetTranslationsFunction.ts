595a1d06da4d6e3c9790b5874a00dbd35f31e8ac
Julian Klummer <secure@julianklummer.de>
2025-04-08 16:36:14 +0200

    fix type

--- next/src/components/types/GetTranslationsFunction.ts
+++ next/src/components/types/GetTranslationsFunction.ts
@@ -2,12 +2,5 @@ import { getTranslations } from "next-intl/server";
 import { ComponentProps } from "react";
 
 export type GetTranslationsFunction<T extends React.FC<any>> = (
-  t: Awaited<
-    ReturnType<
-      typeof getTranslations<{
-        locale: "de" | "en";
-        namespace: "components";
-      }>
-    >
-  >
+  t: Awaited<ReturnType<typeof getTranslations<"components">>>
 ) => ComponentProps<T>["translations"];
